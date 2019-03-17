633a1fcb4cfc3631eccd774a8e76304f85531eda
orderly999 <48650999+orderly999@users.noreply.github.com>
2019-03-17 16:59:39 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,42 @@
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
+	
+	
+	
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
-
+		while(true)
+		{
+			String[] dayOfaWeekStr = {"", "Niedziela","Poniedzia³ek","Wtorek","Œroda","Czwartek","Pi¹tek","Sobota"};
+			Scanner scanner = new Scanner(System.in);
+	        System.out.println("Wpisz datê w formacie dd/mm/yyyy");
+	        String dateString = scanner.nextLine();
+	        System.out.println("Wpisana data to: " + dateString);    
+	        SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+	        Date date = null;
+			try {
+				date = formatDate.parse(dateString);
+			} catch (ParseException e) {
+				System.out.println("Z³y format daty! Podaj poprawny format daty.");
+				return; // zeby uniknac wyrzucenia wyjatku w konsoli
+			}				
+			GregorianCalendar c = (GregorianCalendar) GregorianCalendar.getInstance();
+	        c.setTime(date);
+	        int dayOfaWeekInt = c.get(GregorianCalendar.DAY_OF_WEEK);      
+	        System.out.println(dateString + " to " +  dayOfaWeekStr[dayOfaWeekInt]); 
+	        c.add(GregorianCalendar.YEAR, 1); // dodajemy rok
+	        dayOfaWeekInt = c.get(GregorianCalendar.DAY_OF_WEEK);  
+	        System.out.println("Za rok tego dnia bêdziemy mieli: " + dayOfaWeekStr[dayOfaWeekInt]); 
+		}
 	}
 
 }
